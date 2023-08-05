f34e5e208c68527059332482c0793b4ada25753e
rweisshaar1 <robbieweisshaar@gmail.com>
2023-08-04 19:18:04 -0500

    finalized code

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,9 +1,35 @@
 const router = require('express').Router();
-const data = require('../db/db.json')
+const fs = require("fs");
+const {v4: uuidv4} = require('uuid');
 
+// We should get all the notes and send them back
 router.get("/", (req, res) => {
-  res.status(200).json(data)
+  // readFile to get the latest version of the all the notes
+  fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    const jsonNotes = JSON.parse(data)
+    err ? console.error(err) : res.status(200).json(jsonNotes)
+  })
 });
+
+router.post('/', (req, res) => {
+  // read the current data 
+  // add the new note to that data 
+  // write that data to the same file
+  fs.promises.readFile("./db/db.json", "utf-8")
+  .then(function(data){
+    const jsonNotes = JSON.parse(data);
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuidv4()
+    }
+    jsonNotes.push(newNote);
+    return fs.promises.writeFile("./db/db.json", JSON.stringify(jsonNotes));
+  })
+  .then(function(data){
+    res.json({message: "Job Done!"})
+  })
+})
 
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,19 @@
 const express = require('express'); // gets express working
 const path = require('path');
 const PORT = process.env.PORT || 3001; // determines a port
-const app = express(); // creates a variable to express to use
+const app = express(); // creates a variable for express to use
 const api = require('./routes/index') ; // connects the server side routes in routing folder
-const data = require('./db/db.json');
-const fs = require("fs");
 
 
-app.use("/api", api);
+
+
 
 app.use(express.static('public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api", api);
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
@@ -21,11 +21,6 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
-app.post('/api/notes' , (req, res) => {
-  console.info(`${req} request received to add a review`);
-  
-})
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
